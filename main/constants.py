97ca5baa157e276d38c9b6ddffe03308d70dfa56
Gautam Gurrala <114209399+23gautam@users.noreply.github.com>
2024-04-20 10:52:20 +0530

    Add files via upload

---
+++ main/constants.py
@@ -0,0 +1,14 @@
+email = "samikp149@gmail.com"
+email_password = "tpje pzaw ysoo ffbn"
+#email = "sebtsu@outlook.com"
+#email_password = "barkin2018*"
+
+PAPAERSUBMIT_INITIAL = "Not Submitted"
+PAPAERSUBMIT_SUBMITTED = "Submitted"
+PAPERSUBMIT_ABSTRACT_REJECTED = "abstract_rejected"
+PAPERSUBMIT_ABSTRACT_PENDING = "abstract_pending"
+PAPERSUBMIT_ABSTRACT_ACCEPTED = "abstract_accepted"
+
+PAPERSUBMIT_PAPER_REJECTED = "paper_rejected"
+PAPERSUBMIT_PAPER_PENDING = "paper_pending"
+PAPERSUBMIT_PAPER_ACCEPTED = "paper_accepted"
